e47e80604e423e3ab1b7b653f8d5e8f2136a5bdb
Chen <overwindows@icloud.com>
2019-08-11 21:19:40 +0800

    Gray Code
    Gray Code

---
+++ 89.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        xor_lst = [2**i for i in range(n)]
+        
+        res = []
+        for x in range(2**n):
+            if len(res) == 0:
+                res.append(x)
+            else:
+                prev = res[-1]
+                for e in xor_lst:
+                    if prev^e in res:
+                        continue
+                    else:
+                        res.append(prev^e)
+                        break
+        
+        return res
