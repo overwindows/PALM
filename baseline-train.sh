fcbdc4c260ac2662207840da15211534eebfebf5
Chen Wu <wuchen@Chen-VM.xx0c4qiuisruhf0r2eetf3scib.bx.internal.cloudapp.net>
2020-12-11 05:36:08 +0000

    Add vanilla model

---
+++ baseline-train.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=3 fairseq-train \
+    /datadrive/cnn_dm-bin \
+    --arch transformer --share-decoder-input-output-embed \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --dropout 0.3 --weight-decay 0.0001 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --max-tokens 4096 --skip-invalid-size-inputs-valid-test \
+    --eval-bleu \
+    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-detok moses \
+    --eval-bleu-remove-bpe \
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
+    --eval-bleu-print-samples \
+    --save-dir /datadrive/palm_checkpoints \
+    --tensorboard-logdir /datadrive/palm_logdir
